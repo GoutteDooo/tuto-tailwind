d2d15c12d56fc76e7b5554c6c96b5de9ab178d88
GoutteDooo <goudal.Donovan@live.fr>
2024-11-21 22:55:34 +0100

    test: test tailwind 1st time.

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import tailwind from "eslint-plugin-tailwindcss";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  [...tailwind.configs["flat/recommended"]],
+];
--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,3 @@
+const x = 12;
 
-const button = document.createElement("button");
-
-button.textContent = "Click me!";
-
-button.classList.add("bg-yellow-500", "text-black");
-
-document.body.appendChild(button);
+console.log(x);
